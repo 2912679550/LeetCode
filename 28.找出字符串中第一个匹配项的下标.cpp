8ff90d61b51dfd04f20049b850d46eddc7b7b901
Wu·Zhiyuan-HUST <73233503+2912679550@users.noreply.github.com>
2025-08-15 14:38:27 +0800

    初始化力扣刷题仓库

---
+++ 28.找出字符串中第一个匹配项的下标.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=cpp
+ *
+ * [28] 找出字符串中第一个匹配项的下标
+ */
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+    }
+};
+// @lc code=end
+
